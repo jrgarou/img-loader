636f8b408c40a6ca82092e653050e0a30f1254e9
jrgarou <jrgarou@gmail.com>
2017-07-05 00:23:53 -0300

    update example

--- main.js
+++ main.js
@@ -6,27 +6,34 @@
     async: true,
     images: [
       'http://www.sekonic.com/portals/0/articles/zuckerman_on_landscapes_shooting_the_sun_5-7.jpg',
-      'http://www.sekonic.com/portals/0/articles/zuckerman_on_landscapes_shooting_the_sun_5-4.jpg',
-      'http://www.sekonic.com/portals/0/articles/zuckerman_on_landscapes_shooting_the_sun_5-3.jpg',
+      'http://www.sekonic.com/portals/0/articles/zuckerman_on_landscapes_shooting_the_sun_5-9.jpg',
+      'http://www.sekonic.com/portals/0/articles/zuckerman_on_landscapes_shooting_the_sun_5-10.jpg',
       'http://www.sekonic.com/portals/0/articles/zuckerman_on_landscapes_shooting_the_sun_5-2.jpg',
       'http://www.sekonic.com/portals/0/articles/zuckerman_on_landscapes_shooting_the_sun_5-1.jpg',
     ],
     oncomplete: function (images) {
-      createContent(mycontent, 'Done!', images);
+      createContent('entry two', mycontent, 'Done! All images have been loaded', images);
     },
     onimageloaded: function (response) {
-      createContent(mycontent, 'Loaded ' + (Number(response.index) + 1) + '/' + setup.images.length + ' - ' + response.image.src, response);
+      createContent('entry', mycontent, (Number(response.index) + 1) + '/' + setup.images.length + ' <br> ' + response.image.src, response);
     }
   };
 
-  function createContent(container, text, response) {
-    var el = document.createElement('p');
-    el.innerHTML = text;
-    container.appendChild(el);
-    if(response.image) container.appendChild(response.image);
+  function createContent(klass, container, text, response) {
+    var div = document.createElement('div'),
+        p = document.createElement('p')
+    ;
+
+    p.innerHTML = text;
+    div.appendChild(p);
+    div.className = klass;
+
+    if(response.image) div.appendChild(response.image);
+    container.appendChild(div);
     console.log(response);
   }
 
   var loadimages = new ImgLoader(setup);
+  createContent('entry two', mycontent, 'Start load...', false);
   loadimages.start();
 }());
